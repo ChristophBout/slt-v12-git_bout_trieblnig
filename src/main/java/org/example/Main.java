2f35182f86c209f346b91f945dc44f67b2f54b28
FelixTri <felix.trieblnig@gmail.com>
2024-05-23 15:03:08 +0200

    Added "Hello Felix"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Christoph!");
+        System.out.println("Hello Felix");
     }
+
 }
